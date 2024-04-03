a8734900e777b8e16746214d58d17e495b2b50c1
SyifaZahra <zahraassyifa2006@gmail.com>
2024-04-03 11:32:46 +0700

    image slider

--- markups-outing/navbar.php
+++ markups-outing/navbar.php
@@ -82,37 +82,37 @@
             <div class="wrapper swiper-wrapper">
                 <div class="slide swiper-slide">
                     <img src="assets/images/depankKE.jpg" alt="" class="image" />
-                    <!-- <div class="image-data">
-             <span class="text">Enjoy the finest coffee drinks.</span>
-             <h2>
-               Enjoy Our Exclusive <br />
-               Coffee and Cocktails
-             </h2> -->
-                    <a href="#" class="button">About Us</a>
+                    <div class="image-data">
+                        <span class="text">Enjoy the finest coffee drinks.</span>
+                        <h2>
+                            Enjoy Our Exclusive <br />
+                            Coffee and Cocktails
+                        </h2>
+                        <a href="#" class="button">About Us</a>
+                    </div>
                 </div>
-            </div>
-            <div class="slide swiper-slide">
-                <img src="assets/images/idk.jpg" alt="" class="image" />
-                <div class="image-data">
-                    <span class="text">We really like what we do.</span>
-                    <h2>
-                        Coffee Beans with a <br />
-                        Perfect Aroma
-                    </h2>
-                    <a href="#" class="button">About Us</a>
+                <div class="slide swiper-slide">
+                    <img src="assets/images/idk.jpg" alt="" class="image" />
+                    <div class="image-data">
+                        <span class="text">We really like what we do.</span>
+                        <h2>
+                            Coffee Beans with a <br />
+                            Perfect Aroma
+                        </h2>
+                        <a href="#" class="button">About Us</a>
+                    </div>
                 </div>
-            </div>
-            <div class="slide swiper-slide">
-                <img src="assets/images/y.jpg" alt="" class="image" />
-                <div class="image-data">
-                    <span class="text">Making Our coffee with lover.</span>
-                    <h2>
-                        Alluring and Fragrant <br />
-                        Coffee Aroma
-                    </h2>
-                    <a href="#" class="button">About Us</a>
+                <div class="slide swiper-slide">
+                    <img src="assets/images/y.jpg" alt="" class="image" />
+                    <div class="image-data">
+                        <span class="text">Making Our coffee with lover.</span>
+                        <h2>
+                            Alluring and Fragrant <br />
+                            Coffee Aroma
+                        </h2>
+                        <a href="#" class="button">About Us</a>
+                    </div>
                 </div>
-            </div>
             </div>
 
             <div class="swiper-button-next nav-btn"></div>
